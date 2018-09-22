8df4beba367398fa6ab60e3e70dfcf2fa8400e41
Tamal Sen <tamal.2k14@gmail.com>
2018-09-22 18:57:55 +0600

    Added model, service support for TravelodeMediaCategory

--- app/shared/services/services.module.ts
+++ app/shared/services/services.module.ts
@@ -4,13 +4,15 @@ import { UserService } from "./user.service";
 import { TravelodeService } from "./travelode.service";
 import { MediaService } from "./media.service";
 import { TravelodeMediaService } from "./travelode-media.service";
+import { TravelodeMediaCategoryService } from "./travelode-media-category.service";
 
 @NgModule({
     providers: [
         UserService,
         TravelodeService,
         MediaService,
-        TravelodeMediaService
+        TravelodeMediaService,
+        TravelodeMediaCategoryService
     ]
 })
 
